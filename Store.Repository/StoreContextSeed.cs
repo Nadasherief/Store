f4993c8d53cbe9b8706f11282fa92241c457c64b
NadaSherief <nndyshryfhsyn@gmail.com>
2024-10-12 15:42:19 +0300

    Repository Layer And Seed Data

---
+++ Store.Repository/StoreContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Store.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository
+{
+    public class StoreContextSeed
+    {
+        public static async  Task SeedAsync(StoreDbContext StoreDbContext, ILoggerFactory loggerFactory )
+        {
+            try
+            {
+                if (StoreDbContext.productBrands != null && !StoreDbContext.productBrands.Any()) 
+                {
+                    //var brandData=
+                }
+                else { }
+            }
+            catch (Exception ex) {
+            }
+        } 
+    }
+}
