ebccecca5dd5f8d226064999cc6d0c5f156f0068
NadaSherief <nndyshryfhsyn@gmail.com>
2024-10-12 17:55:12 +0300

    Generic Repository

---
+++ Store.Repository/InterFace/IGenaricReposiatory.cs
@@ -0,0 +1,19 @@
+﻿using Store.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.InterFace
+{
+    public interface IGenaricReposiatory<TEntity , TKey> where TEntity : BaseEntity<TKey>
+    { 
+        Task<TEntity> GetByIDAsync(TKey? id);
+        Task<IReadOnlyList<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        void UpdateAsync(TEntity entity);
+        void DeleteAsync(TEntity entity);
+    }
+}
